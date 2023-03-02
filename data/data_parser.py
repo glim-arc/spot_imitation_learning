b7f6d4319230d2be85469b20dca99ecb0d3e6a99
g.lim <lim215@purdue.edu>
2023-03-02 18:09:22 -0500

    update

--- data/data_parser.py
+++ data/data_parser.py
@@ -16,25 +16,42 @@ def main():
     """Extract a folder of images from a rosbag.
     """
     parser = argparse.ArgumentParser(description="Extract images from a ROS bag.")
-    parser.add_argument("bag_file",type=str, default='./',  help="Input ROS bag.")
+    parser.add_argument("bag_file",type=str, default='./library2pond_spot.bag',  help="Input ROS bag.")
     parser.add_argument("output_dir", type=str, default='./output', help="Output directory.")
-    parser.add_argument("image_topic", help="Image topic.")
+
+    imgtopics = ["/spot/camera/back/image/compressed",
+             "/spot/camera/frontleft/image/compressed",
+             "/spot/camera/frontright/image/compressed",
+             "/spot/camera/left/image/compressed",
+             "/spot/camera/right/image/compressed"]
+    
+    joystick_input = ["/joystick"]
+    localization = ["/localization"]
 
     args = parser.parse_args()
-
-    print("Extract images from %s on topic %s into %s" % args.bag_file,
-                                                          args.image_topic, args.output_dir)
 
     bag = rosbag.Bag(args.bag_file, "r")
     bridge = CvBridge()
     count = 0
-    for topic, msg, t in bag.read_messages(topics=[args.image_topic]):
-        cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    
+    for savedtopic in imgtopics:
+        print(savedtopic)
 
-        cv2.imwrite(os.path.join(args.output_dir, "frame%06i.png" % count), cv_img)
-        print("Wrote image %i" % count)
+        camloc = savedtopic.find("/spot/camera/")
+        lastidx = camloc
+        while camloc[lastidx] != '/':
+            lastidx += 1
 
-        count += 1
+        curdir = "./" + savedtopic[camloc+1:lastidx]
+        os.makedirs(curdir)
+
+        for topic, msg, t in bag.read_messages(topics=[savedtopic]):
+            cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+
+            cv2.imwrite(os.path.join(curdir, "frame%06i.png" % count), cv_img)
+            print("Wrote image %i" % count)
+
+            count += 1
 
     bag.close()
 
