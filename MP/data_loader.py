4f0cd7f450ad407d29a359f6c64fd6a6e4542930
glim <lim215@purdue.edu>
2023-04-09 13:47:36 -0400

    data loader completed

---
+++ MP/data_loader.py
@@ -0,0 +1,75 @@
+import numpy as np
+import os
+import os.path
+import csv
+import pandas as pd
+from data.lidar_parser import parse_lidar
+
+class Bag:
+    def __init__(self,name, odom_time, pos, ori, twist_lin, twist_angular,pcl_time,obs_list):
+        self.name = name
+        self.odom_time = odom_time
+        self.pos = pos
+        self.ori = ori
+        self.twist_lin = twist_lin
+        self.twist_angular = twist_angular
+        self.pcl_time = pcl_time
+        self.obs_list = obs_list #list
+def load_obs_from_lidar(path):
+    pcl_list = os.listdir(path)
+    time = [float(curpcl[:-4]) for curpcl in pcl_list]
+    time.sort()
+    time = np.array(time)
+
+    obs_list = []
+
+    print("Start pcl parsing")
+    for curpcl in time:
+        print(curpcl)
+        curpclpath = os.path.join(path, str(curpcl)+".pcd")
+        cur_obs = parse_lidar(curpclpath)
+        obs_list.append(cur_obs)
+    print("End pcl parsing")
+
+    return time, obs_list
+
+def load_dynamical_inputs(path):
+    df = pd.read_csv(path)
+
+    df.drop(df.columns[1:6], axis=1, inplace=True)
+    df.drop(["pose.covariance", "twist.covariance"], axis=1, inplace=True)
+
+    time = np.array(df['Time'])
+    pos = np.array([df['pose.pose.position.x'],df['pose.pose.position.y'],df['pose.pose.position.z']])
+    ori = np.array([df['pose.pose.orientation.x'],df['pose.pose.orientation.y'],df['pose.pose.orientation.z']])
+    twist_lin = np.array([df['twist.twist.linear.x'],df['twist.twist.linear.y'],df['twist.twist.linear.z']])
+    twist_angular = np.array([df['twist.twist.linear.x'],df['twist.twist.linear.y'],df['twist.twist.linear.z']])
+
+    print("odom loaded")
+
+    return time, pos, ori, twist_lin, twist_angular
+
+def load_dataset():
+    current_path = os.getcwd()
+    current_path = os.path.join(current_path, "data/bag")
+    bagfilelist = os.listdir(current_path)
+
+    for i,bagfile in enumerate(bagfilelist):
+        if os.path.isfile(os.path.join(current_path, bagfile)):
+            bagfilelist.pop(i)
+
+    bag_list = []
+
+    for bagname in bagfilelist:
+        print(bagname, "parsing start")
+        cur_dir = os.path.join(current_path, bagname)
+        odom_path = os.path.join(cur_dir, "odom.csv")
+        pcl_path = os.path.join(cur_dir, "velodyne_points")
+        odom_time, pos, ori, twist_lin, twist_angular =  load_dynamical_inputs(odom_path)
+        pcl_time,obs_list = load_obs_from_lidar(pcl_path)
+        bag_list.append(Bag(bagname,odom_time, pos, ori, twist_lin, twist_angular,pcl_time,obs_list))
+
+    return bag_list
+
+#load_obs_from_lidar("./data/bag/A_Spot_Jester_Jester_Wed_Nov_10_64/velodyne_points")
+load_dataset()
---
+++ data/lidar_parser.py
@@ -0,0 +1,99 @@
+import open3d
+import math
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+def parse_lidar(path):
+    #curcloud = open3d.io.read_point_cloud("./1636570668.255584.pcd")
+    curcloud = open3d.io.read_point_cloud(path)
+
+    # bounding box:
+    bounds = [[0, 3], [-3, 3], [-0.3, 5]]
+    box_section = list(itertools.product(*bounds))
+    bounding_box = open3d.geometry.AxisAlignedBoundingBox.create_from_points(
+    open3d.utility.Vector3dVector(box_section))
+    curcloud = curcloud.crop(bounding_box)
+
+    #stat_outlier
+    cropped_cloud, in_pts = curcloud.remove_statistical_outlier(nb_neighbors=200, std_ratio=1.6)
+    in_cld = curcloud.select_by_index(in_pts)
+    out_cld = curcloud.select_by_index(in_pts, invert=True)
+    in_cld.paint_uniform_color([0.8, 0, 0])
+    out_cld.paint_uniform_color([0.3, 1, 1])
+
+    pcd = in_cld
+
+    with open3d.utility.VerbosityContextManager(
+            open3d.utility.VerbosityLevel.Debug) as cm:
+        labels = np.array(
+            pcd.cluster_dbscan(eps=0.4, min_points=50, print_progress=True))
+
+    max_label = labels.max()
+    clustered_point = [[] for i in range(max_label+1)]
+    center = [[] for i in range(max_label+1)]
+
+    for i, label_num in enumerate(labels):
+        clustered_point[label_num].append(i)
+
+    boxes = []
+    obstacle_pts = []
+
+    print(f"point cloud has {max_label + 1} clusters")
+    # colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+    # colors[labels < 0] = 0
+    # pcd.colors = open3d.utility.Vector3dVector(colors[:, :3])
+    # open3d.visualization.draw_geometries([pcd])
+
+    for i, cur_cluster in enumerate(clustered_point):
+        cur_cluster_pts = [pcd.points[pts] for pts in cur_cluster]
+        cluster_vector = open3d.utility.Vector3dVector(cur_cluster_pts)
+        bounding_box = open3d.geometry.AxisAlignedBoundingBox.create_from_points(cluster_vector)
+        bounding_box.color = [1,0,0]
+        boxes.append(bounding_box)
+
+        center[i] = bounding_box.get_center().tolist()
+        # verticies = bounding_box.get_box_points()[3:5]
+
+        # numstep = math.sqrt((verticies[0][0] - verticies[1][0])**2 + (verticies[0][1] - verticies[1][1])**2) // 1
+
+        # obsposlist = [center[i]]
+
+        # if numstep == 0:
+        #     obstacle_pts += obsposlist
+        #     continue
+
+        # xstep = (verticies[0][0] - verticies[1][0])/numstep
+        # ystep = (verticies[0][1] - verticies[1][1])/numstep
+
+        # xtemp = verticies[1][0]
+        # ytemp = verticies[1][1]
+
+        # obsposlist.append([xtemp, ytemp, center[i][2]])
+
+        # for k in range(int(numstep)):
+        #     xtemp += xstep
+        #     ytemp += ystep
+        #     obspos = [xtemp, ytemp, center[i][2]]
+        #     obsposlist.append(obspos)
+
+        # obstacle_pts += obsposlist
+        # obsposlist = np.array(obsposlist)
+
+        # tempcloud = open3d.geometry.PointCloud()
+        # tempcloud.points = cluster_vector
+        # obscloud = open3d.geometry.PointCloud()
+        # obscloud.points=open3d.utility.Vector3dVector(obsposlist)
+        # obscloud.paint_uniform_color([0.8, 0, 0])
+        # open3d.visualization.draw_geometries([tempcloud, obscloud])
+
+    # obsposlist = np.array(obstacle_pts)
+    # obsposlist = np.array(center)
+    # obscloud = open3d.geometry.PointCloud()
+    # obscloud.points=open3d.utility.Vector3dVector(obsposlist)
+    # obscloud.paint_uniform_color([0.2, 0, 0])
+    # open3d.visualization.draw_geometries([obscloud] + boxes)
+
+    return center
+
+#parse_lidar("./1636570668.255584.pcd")
