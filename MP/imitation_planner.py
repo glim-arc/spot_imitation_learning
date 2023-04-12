77af0a18471d0d9630f2da1488cb9777a36b5d20
Your Name <you@example.com>
2023-04-11 22:32:58 -0400

    Model revised

--- MP/imitation_planner.py
+++ MP/imitation_planner.py
@@ -14,17 +14,27 @@ import argparse
 class ImitationPlanner(nn.Module):
     def __init__(self, input_size, output_size):
         super().__init__()
+        p = 0.2
         self.fc = nn.Sequential(
-            nn.Linear(input_size, 2048),nn.PReLU(),
-            nn.Linear(2048, 1024),nn.PReLU(),nn.Dropout(),
-            nn.Linear(1024, 896),nn.PReLU(),
-            nn.Linear(896, 768),nn.PReLU(),
-            nn.Linear(768, 512),nn.PReLU(),
-            nn.Linear(512, 384),nn.PReLU(),
-            nn.Linear(384, 256),nn.PReLU(),
-            nn.Linear(256, 128),nn.PReLU(),
-            nn.Linear(128, 64),nn.PReLU(),
-            nn.Linear(64, 32),nn.PReLU(),
+            nn.Linear(input_size, 1280),nn.PReLU(),nn.Dropout(p=p),
+            nn.BatchNorm1d(1280),
+            nn.Linear(1280, 1024),nn.PReLU(),nn.Dropout(p=p),
+            nn.BatchNorm1d(1024),
+            nn.Linear(1024, 896),nn.PReLU(),nn.Dropout(p=p),
+            nn.BatchNorm1d(896),
+            nn.Linear(896, 768),nn.PReLU(),nn.Dropout(p=p),
+            nn.BatchNorm1d(768),
+            nn.Linear(768, 512),nn.PReLU(),nn.Dropout(p=p),
+            nn.BatchNorm1d(512),
+            nn.Linear(512, 384),nn.PReLU(),nn.Dropout(p=p),
+            nn.BatchNorm1d(384),
+            nn.Linear(384, 256),nn.PReLU(), nn.Dropout(p=p),
+            nn.BatchNorm1d(256),
+            nn.Linear(256, 128),nn.PReLU(), nn.Dropout(p=p),
+            nn.BatchNorm1d(128),
+            nn.Linear(128, 64),nn.PReLU(), nn.Dropout(p=p),
+            nn.BatchNorm1d(64),
+            nn.Linear(64, 32),nn.PReLU(), nn.Dropout(p=p),
             nn.Linear(32, output_size))
 
     def forward(self, x):
